40b86a13f6035347e957cf2d6d0657c59fa6f569
Juan Noguera <janm9709@gmail.com>
2020-11-06 16:53:56 -0500

    tercer commmit

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
-const { response } = require('express');
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
 
 app.get('/', (req, res) => {
-    // res.send('Hola Mundo');
-    let salida = {
-        nombre: 'Juan',
-        edad: 24,
-        url: req.url
-    };
-    res.send(salida);
+    res.render('home', {
+        nombre: 'juAn'
+    });
+});
+app.get('/about', (req, res) => {
+    res.render('about', {
+
+    });
 });
 
-app.listen(3000, () => {
-    console.log('Escuchando peticiones en el puerto 3000');
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
 });
